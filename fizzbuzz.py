454562d6b74b923495caa5d55845d7b0937a7945
chikara <chikara.f.yoshida@gmail.com>
2018-11-17 16:34:25 +0900

    fizzbuzz_convert 関数を変更 出力処理を関数外に出した

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,19 @@
-number = int(input('1つの自然数を入れてね；'))
+def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return 'Fizz'
+    elif number % 5 == 0:
+        return 'Buzz'
+    else:
+        return str(number)
 
-if number % 15 == 0:
-    print("FizzBuzz")
-elif number % 3 == 0:
-    print('Fizz')
-elif number % 5 == 0:
-    print('Buaa')
-else:
-    print(str(number))
+
+number = 1
+
+while True:
+    print(fizzbuzz_convert(number))
+    number += 1
+
+    if number == 101:
+        break
