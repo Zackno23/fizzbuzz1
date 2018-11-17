1d60520fc52d253db0bb65bdbad1de0c67d418d9
chikara <chikara.f.yoshida@gmail.com>
2018-11-17 14:43:06 +0900

    3の倍数のときにfizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,7 @@
 number = int(input('1つの自然数を入れてね；'))
-str(number)
+if number % 3 == 0:
+    output = 'Fizz'
+else:
+    output = str(number)
 
-print(str(number))
-
-print(type(number))
+print(output)
