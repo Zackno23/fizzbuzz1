0f47283f1f70ebdcb72473849efb9c11d222c67c
chikara <chikara.f.yoshida@gmail.com>
2018-11-17 16:48:22 +0900

    必要ない処理を削除

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,19 +1,24 @@
 def fizzbuzz_convert(number):
     if number % 15 == 0:
         return "FizzBuzz"
-    elif number % 3 == 0:
+    if number % 3 == 0:
         return 'Fizz'
-    elif number % 5 == 0:
+    if number % 5 == 0:
         return 'Buzz'
-    else:
-        return str(number)
+    return str(number)
 
 
-number = 1
-
+"""
 while True:
     print(fizzbuzz_convert(number))
     number += 1
 
     if number == 101:
         break
+
+for i in range(100):
+    print(fizzbuzz_convert(number))
+"""
+
+for i in range(100):
+    print(fizzbuzz_convert(i))
